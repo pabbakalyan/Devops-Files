eff64009ada7fd7e3baac09a2048dfafddc72ec0
pabbakalyan <119713183+pabbakalyan@users.noreply.github.com>
2023-10-12 17:44:20 +0530

    Create  NEXUS-INSTALLATION-1.sh

---
+++ Installation-Tools/NEXUS-INSTALLATION-1.sh
@@ -0,0 +1,20 @@
+------------------ NEXUS INSTALLATION  ----------------------
+Prerequisites:
+wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+
+tar -xf latest-unix.tar.gzConfigure Nexus:
+
+cd nexus
+cd bin/
+
+chmod +x nexus
+
+./nexus start
+
+Open a web browser and navigate to http://localhost:8081. This is the default URL for accessing Nexus.
+cat /home/ubuntu/sonatype-work/nexus3/admin.password
+
+------------------------------OR ELSE DIRECTLY ------------------------------------------------
+
+Install Nexus using Docker
+docker run -d --name nexus -p 8081:8081 sonatype/nexus3
